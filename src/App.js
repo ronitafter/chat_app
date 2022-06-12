c9c8e697702461ac3494bccc23e7ce10d9cc2e2b
ronitafter <54575125+ronitafter@users.noreply.github.com>
2022-06-12 17:57:47 +0300

    test 4

--- src/App.js
+++ src/App.js
@@ -121,7 +121,7 @@ function ChatMessage(props) {
 
   return (<>
     <div className={`message ${messageClass}`}>
-      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
+      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} alt="aaa" />
       <p>{text}</p>
     </div>
   </>)
